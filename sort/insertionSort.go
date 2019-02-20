623268f7ba70e871437c93e221da1ae2ff0fee4e
Ting Lee <tingtingdisco@gmail.com>
2019-02-21 01:14:52 +0800

    Initial commit

---
+++ sort/insertionSort.go
@@ -0,0 +1,11 @@
+package sort
+
+func insertionSort(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		for j := i; j > 0; j-- {
+			if arr[j] < arr[j-1] {
+				arr[j], arr[j-1] = arr[j-1], arr[j]
+			}
+		}
+	}
+}
